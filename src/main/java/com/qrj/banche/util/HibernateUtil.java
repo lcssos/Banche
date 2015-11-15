21a62cc5fb37c97ab3dfaec0a74af8e087f49b4e
lcssos <lcs@hh-medic.com>
2015-11-16 00:16:42 +0800

    将Hibernate替换为Mybatis

--- src/main/java/com/qrj/banche/util/HibernateUtil.java
+++ src/main/java/com/qrj/banche/util/HibernateUtil.java
@@ -1,23 +1,23 @@
-package com.qrj.banche.util;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
-public class HibernateUtil {
-    private static SessionFactory sf;
-
-    static {
-        Configuration cfg = new Configuration().configure();
-        StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder()
-                .applySettings(cfg.getProperties());
-        StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder
-                .build();
-        sf = cfg.buildSessionFactory(standardServiceRegistry);
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sf;
-    }
-}
+//package com.qrj.banche.util;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+//
+//public class HibernateUtil {
+//    private static SessionFactory sf;
+//
+//    static {
+//        Configuration cfg = new Configuration().configure();
+//        StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder()
+//                .applySettings(cfg.getProperties());
+//        StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder
+//                .build();
+//        sf = cfg.buildSessionFactory(standardServiceRegistry);
+//    }
+//
+//    public static SessionFactory getSessionFactory() {
+//        return sf;
+//    }
+//}
